cb8031bb04f0182a21276fba26ecd8fffcfdac3e
adrien franto <radrienfranto2@gmail.com>
2025-08-15 21:35:37 +0300

    Question 3: Création de la classe Homme avec champs nom, prenom, age et constructeurs

---
+++ src/main/java/iut/bad/Homme.java
@@ -0,0 +1,17 @@
+package iut.bad;
+
+public class Homme {
+	private String nom;
+	private String prenom;
+	private int age;
+	
+	public Homme() {
+		
+	}
+	
+	public Homme(String nom,String prenom,int age ) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+}
