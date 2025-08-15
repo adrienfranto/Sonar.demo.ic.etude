0ec220fd9d252449c54b814deaf4716653998a78
adrien franto <radrienfranto2@gmail.com>
2025-08-15 22:45:55 +0300

    Question 9: Déplacement et override de la méthode toString() dans Homme et Femme

--- src/main/java/iut/bad/Femme.java
+++ src/main/java/iut/bad/Femme.java
@@ -10,5 +10,10 @@ public class Femme extends Humain {
 	public Femme(String nom,String prenom,int age) {
 		super(nom,prenom,age);
 	}
+	
+	@Override
+	public String toString() {
+		return "Madame " + nom + " , " + prenom + " et " + age;
+	}
 
 }
