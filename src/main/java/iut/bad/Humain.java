d78402af4c975760e8f4e836b159a81c1850f8d7
adrien franto <radrienfranto2@gmail.com>
2025-08-16 00:03:43 +0300

    Refactor Humain: Replace System.out with Logger for SonarQube compliance

--- src/main/java/iut/bad/Humain.java
+++ src/main/java/iut/bad/Humain.java
@@ -1,49 +1,51 @@
 package iut.bad;
 
+import java.util.logging.Logger;
+
 public class Humain implements Consommation {
-	protected String nom;
-	protected String prenom;
-	protected int age;
-	
-	public Humain() {
-		super();
-	}
-	
-	public Humain(String nom,String prenom,int age) {
-		super();
-		this.nom = nom;
-		this.prenom = prenom;
-		this.age = age;
-	}
-	
-	public void details() {
-		System.out.println(toString());
-	}
+    private static final Logger LOGGER = Logger.getLogger(Humain.class.getName());
 
-	@Override
-	public String toString() {
-		return nom + " , " + prenom + " et " + age;
-	}
-	
-	
-	@Override
-	public void manger() {
-	    System.out.println(nom + " " + prenom + " prend le temps de manger.");
-	}
+    protected String nom;
+    protected String prenom;
+    protected int age;
 
-	@Override
-	public void boire() {
-	    System.out.println(nom + " " + prenom + " s'accorde un moment pour boire.");
-	}
-	
-	public void ami(Humain h) {
+    public Humain() {
+        super();
+    }
+
+    public Humain(String nom,String prenom,int age) {
+        super();
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public void details() {
+        LOGGER.info(toString());
+    }
+
+    @Override
+    public String toString() {
+        return nom + " , " + prenom + " et " + age;
+    }
+
+    @Override
+    public void manger() {
+        LOGGER.info(nom + " " + prenom + " prend le temps de manger.");
+    }
+
+    @Override
+    public void boire() {
+        LOGGER.info(nom + " " + prenom + " s'accorde un moment pour boire.");
+    }
+
+    public void ami(Humain h) {
         ami(h, 100);
     }
 
     public void ami(Humain h, int duree) {
-        System.out.println(this.nom + " " + this.prenom +
-                           " est maintenant ami avec " + h.nom + " " + h.prenom +
-                           " pour " + duree + " jours.");
+        LOGGER.info(this.nom + " " + this.prenom +
+                    " est maintenant ami avec " + h.nom + " " + h.prenom +
+                    " pour " + duree + " jours.");
     }
-
 }
